c88e73398298d85a930816611d9fa0aedf3c6c92
davide <davide.lucchi@studenti.unipd.it>
2017-07-10 18:56:43 +0200

    final version ready for upload

--- dbTwitter/script/gradle/executing.sh
+++ dbTwitter/script/gradle/executing.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -cp ../../libs/postgresql-42.1.1.jar:../../build/libs/db-project-1.0-SNAPSHOT-all.jar it.unipd.dei.db.ClusTweets >log.txt
+java -cp ../../libs/postgresql-42.1.1.jar:../../build/libs/db-project-1.0-SNAPSHOT-all.jar:../../libs/stanford-postagger.jar:../../libs/stanford-postagger-3.8.0.jar:../../libs/stanford-postagger-3.8.0-javadoc.jar:../../libs/stanford-postagger-3.8.0-sources.jar it.unipd.dei.db.ClusTweets >log.txt
